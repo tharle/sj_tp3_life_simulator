a5a75f5da3e1e3e7ab5288c45d0c1a359e953481
Tharle Josefi de Camargo <tharlejc@gmail.com>
2024-04-17 12:14:55 -0400

    Add camera 3p

---
+++ Assets/Scripts/Camera/CameraMusicController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMusicController : MonoBehaviour
+{
+
+
+    // TODO: Transformer ça dans Event quand j'ai fini le jeu
+    void Update()
+    {
+        if(!PlayerController.Instance.IsPlaying) GetComponent<AudioSource>().Stop();
+    }
+}
